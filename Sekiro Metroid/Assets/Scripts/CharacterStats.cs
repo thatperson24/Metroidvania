50c179d7bba3001ff169adf90824396639a4448f
thatperson24 <pengdis24@gmail.com>
2022-08-31 20:23:43 -0700

    Enemy and character stats WIP

---
+++ Sekiro Metroid/Assets/Scripts/CharacterStats.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterStats : MonoBehaviour
+{
+    [SerializeField] private int health;
+    [SerializeField] private int stance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
