fbeda6741dab48a478238e0746e58b731da34dfd
thatperson24 <pengdis24@gmail.com>
2022-08-31 19:07:55 -0700

    Character movement created

--- Sekiro Metroid/Assets/Scripts/CharacterMovement.cs
+++ Sekiro Metroid/Assets/Scripts/CharacterMovement.cs
@@ -1,18 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.Events;
 using UnityEngine;
 
-public class CharacterController : MonoBehaviour
+public class CharacterMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	private float horizontal;
+	[SerializeField] private float speed;
+	[SerializeField] private float jumpingPower;
+	private bool isFacingRight = true;
 
-    // Update is called once per frame
+	[SerializeField] private Rigidbody2D rb;
+	[SerializeField] private Transform groundCheck;
+	[SerializeField] private LayerMask groundLayer;
+
     void Update()
     {
-        
+        horizontal = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetButtonDown("Jump") && IsGrounded())
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
+        }
+
+        if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+        }
+
+        Flip();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+    }
+
+    private void Flip()
+    {
+        if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
+        {
+            isFacingRight = !isFacingRight;
+            Vector3 localScale = transform.localScale;
+            localScale.x *= -1f;
+            transform.localScale = localScale;
+        }
     }
 }
