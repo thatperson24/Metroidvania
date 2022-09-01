a2cf6d86da9b84bf8338a1234d58161f23f540e3
thatperson24 <pengdis24@gmail.com>
2022-09-01 03:51:59 -0700

    Added Comments to EnemyController script

--- Sekiro Metroid/Assets/Scripts/EnemyController.cs
+++ Sekiro Metroid/Assets/Scripts/EnemyController.cs
@@ -9,21 +9,23 @@ public class EnemyController : MonoBehaviour
     [SerializeField] private int health;
     [SerializeField] private int stamina;
 
+    //---All attack Stuff
     [SerializeField] private float attackRange;
     [SerializeField] private Transform attackPoint;
     [SerializeField] private LayerMask playerLayer;
 
     [SerializeField] private float attackDelay;
-    [SerializeField] private Collider2D highHB;
+    [SerializeField] private Collider2D highHB; //hitbox for high attacks
     [SerializeField] private SpriteRenderer highSR; //Temporary, will be replaced with animation later
-    [SerializeField] private Collider2D lowHB;
+    [SerializeField] private Collider2D lowHB; //hitbox for low attacks
     [SerializeField] private SpriteRenderer lowSR; //Temporary, will be replaced with animation later
 
     private CharacterStats character;
     private bool isAttacking;
     private bool isAggroed;
     private float attackValue = 10f;
-    // Start is called before the first frame update
+    //-----
+    
     void Start()
     {
         health = maxHealth;
@@ -31,6 +33,7 @@ public class EnemyController : MonoBehaviour
         isAggroed = false;
         isAttacking = false;
 
+        // disabling the attack hitboxes and sprites
         highHB.enabled = false;
         highSR.enabled = false;
         lowHB.enabled = false;
@@ -42,19 +45,20 @@ public class EnemyController : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+        //Checking if player is in range and if enemy is already attacking to prevent multiple attacks
         if (Physics2D.OverlapCircle(attackPoint.position, attackRange, playerLayer) && isAttacking == false)
         {
-            Debug.Log("ahh");
-            StartCoroutine(AttackDelay());
-            
+            StartCoroutine(AttackDelay());   
         }
     }
 
+    //Not used yet but gets aggro'd if player enters trigger
     public void StartAggro()
     {
         isAggroed = true;
     }
 
+    //randomly selects attack options and activates hitbox accordingly
     public void Attack()
     {
         int attackOption = Random.Range(0, 2); //0 is high, 1 is low
@@ -73,6 +77,7 @@ public class EnemyController : MonoBehaviour
         character.Damage(attackValue);
     }
 
+    //Reset all attack hitboxes and isAttacking boolean so enemy can attack again
     public void ResetAttack()
     {
         highHB.enabled = false;
@@ -82,6 +87,7 @@ public class EnemyController : MonoBehaviour
         isAttacking = false;
     }
 
+    //Attack once every delay length
     public IEnumerator AttackDelay()
     {
         Attack();
@@ -89,6 +95,7 @@ public class EnemyController : MonoBehaviour
         ResetAttack();
     }
 
+    //Draws where the attack point is and the radius of it.
     void OnDrawGizmosSelected()
     {
         Gizmos.DrawWireSphere(attackPoint.position, attackRange);
