8d4b52b0ed20157cfbd9ee6acd495f5bdbbcc847
thatperson24 <pengdis24@gmail.com>
2022-09-01 17:01:00 -0700

    Player can now attack enemies

--- Sekiro Metroid/Assets/Scripts/CharacterAttacks.cs
+++ Sekiro Metroid/Assets/Scripts/CharacterAttacks.cs
@@ -11,30 +11,57 @@ public class CharacterAttacks : MonoBehaviour
     }
     private Attacks attackType;
     [SerializeField] private float attackValue;
+    [SerializeField] private float attackDelay;
+    private bool isAttacking;
 
     [SerializeField] private float attackRange;
     [SerializeField] private Transform attackPoint;
     [SerializeField] private LayerMask enemyLayer;
 
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        isAttacking = false;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (Input.GetKey(KeyCode.DownArrow) && Input.GetKey(KeyCode.A))
+        if (isAttacking == false)
         {
-            Debug.Log("Low Attack");
-        }
-        else if (Input.GetKey(KeyCode.A))
-        {
-            Debug.Log("Attacking");
+            if (Input.GetKey(KeyCode.DownArrow) && Input.GetKey(KeyCode.A))
+            {
+                Debug.Log("Low Attack");
+                isAttacking = true;
+                StartCoroutine(AttackDelay());
+            }
+            else if (Input.GetKey(KeyCode.A))
+            {
+                Debug.Log("Attacking");
+                isAttacking = true;
+                StartCoroutine(AttackDelay());
+            }
         }
     }
 
+    private void Attack()
+    {
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayer);
+
+        foreach(Collider2D enemy in hitEnemies)
+        {
+            enemy.GetComponent<CharacterStats>().Damage(attackValue);
+        }
+    }
+
+    //Attack once every delay length
+    public IEnumerator AttackDelay()
+    {
+        Attack();
+        yield return new WaitForSeconds(attackDelay);
+        isAttacking = false;
+    }
     void OnDrawGizmosSelected()
     {
         Gizmos.DrawWireSphere(attackPoint.position, attackRange);
